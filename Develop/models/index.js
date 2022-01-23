01d8616ea0b0c1a8b2449431e66e821d04047434
Richard Rogers <treyrogers073@gmail.com>
2022-01-23 15:07:19 -0700

    setup index.js models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,26 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.hasOne(Category, {
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  throughModel: 'ProductTag',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  throughModel: 'ProductTag',
+});
 
 module.exports = {
   Product,
--- Develop/server.js
+++ Develop/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const routes = require('./routes');
+const mySql = require('mysql2');
 // import sequelize connection
+const sequelize = require('./config/connection');
+require('dotenv');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3306;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -11,6 +14,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {console.log(`App listening on port ${PORT}!`)});
 });
