908e07209d8214dd9383f0a2c2f20eee8f5cf5e6
Richard Rogers <treyrogers073@gmail.com>
2022-01-23 20:06:22 -0700

    completed my category-routes file in my routes/api folder.

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -16,20 +16,49 @@ router.get('/', async (req, res) => {
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
-
   // be sure to include its associated Products
+  Category.findOne(req.params.id).then((categoryData) => {
+    res.json(categoryData)
+  })
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  }).catch((err) => res.status(400).json(err));
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
